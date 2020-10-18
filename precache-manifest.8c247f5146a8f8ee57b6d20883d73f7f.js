d3bef32f6353642a3ac945c516f2a9e9a2fc4867
Nikita <cheremuhinikita@mail.ru>
2020-10-18 13:54:35 +0300

    Updates

---
+++ precache-manifest.8c247f5146a8f8ee57b6d20883d73f7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b96f1789409d856ac10f3507f4d25fcc",
+    "url": "/Admin-Panel/index.html"
+  },
+  {
+    "revision": "0427d2c7950803c42c92",
+    "url": "/Admin-Panel/static/css/main.ed1ecb0d.chunk.css"
+  },
+  {
+    "revision": "1a3c3d731df5d8076487",
+    "url": "/Admin-Panel/static/js/2.aa68b45c.chunk.js"
+  },
+  {
+    "revision": "a64acf8e3ad6aa1497056f0bb9a7ce4f",
+    "url": "/Admin-Panel/static/js/2.aa68b45c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0427d2c7950803c42c92",
+    "url": "/Admin-Panel/static/js/main.a5a69981.chunk.js"
+  },
+  {
+    "revision": "3ba3cfe8020371d56d8f",
+    "url": "/Admin-Panel/static/js/runtime-main.7566118b.js"
+  }
+]);
